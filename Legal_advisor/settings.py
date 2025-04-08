976bab69d8ad41468c9f6e876527e1fedd081a9c
Aleenamol Roy <aleenamolroy2025@mca.ajce.in>
2025-04-08 16:36:10 +0530

    Update settings.py

--- Legal_advisor/settings.py
+++ Legal_advisor/settings.py
@@ -32,9 +32,10 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
      'home',
+     'channels',
 ]
 SITE_ID = 1
-
+ASGI_APPLICATION = 'Legal_advisor.asgi.application'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
@@ -87,6 +88,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Legal_advisor.wsgi.application'
 
+# Razorpay API Keys Y6BonJIr6wHpNMK4eo1NHYoo
+RAZORPAY_KEY_ID = 'rzp_test_AnXj68fT4zKwe0'
+RAZORPAY_KEY_SECRET = 'jo7RcNBDpB5qIYJ3NUBKHeVk'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -94,11 +98,14 @@ WSGI_APPLICATION = 'Legal_advisor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'legal_advisor',  # Replace with your database name
-        'USER': 'root',  # Replace with your MySQL username
-        'PASSWORD':"",  
-        'HOST': 'localhost',  # The IP address of your MySQL server (usually localhost or 127.0.0.1 for XAMPP)
-        'PORT': '3306',  # The port your MySQL server is running on (3306 is the default port for MySQL)
+        'NAME': 'legaladvisor_mightfind',  # Replace with your database name
+        'USER': 'legaladvisor_mightfind',  # Replace with your MySQL username
+        'PASSWORD':"88da7bb635dd3c6524665a0d5e9bbc21e5c60c18",  
+        'HOST': 'wcd96.h.filess.io',  # The IP address of your MySQL server (usually localhost or 127.0.0.1 for XAMPP)
+        'PORT': '61001',  # The port your MySQL server is running on (3306 is the default port for MySQL)
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -120,7 +127,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
